3d8d073dccaad3e18439ac9af97d416873368a1b
koowyzrk <banaszkiewiczjasiek@gmail.com>
2025-03-13 13:12:49 +0100

    test

--- ReactiveInteractiveUserInterface/BusinessLogic/AssemblyInfo.cs
+++ ReactiveInteractiveUserInterface/BusinessLogic/AssemblyInfo.cs
@@ -14,3 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("BusinessLogicTest")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4a734722-dd89-43c0-ae05-70f53f6dfe4b")]
+
